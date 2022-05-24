9d66924ad7bc98d2b56d7ee299fbd736b105ba36
Ruddy García <ruddygasol94@gmail.com>
2022-05-24 12:07:39 -0500

    Initial dynamic forms

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Documento {
+  id: number;
+  nombre: string;
+}
+
+interface Empleado {
+  nombre: string;
+  archivos: Documento[];
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -7,10 +17,35 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DinamicosComponent implements OnInit {
+  nuevoArchivo: string = '';
+  empleado: Empleado = {
+    nombre: 'Ruddy',
+    archivos: [
+      { id: 1, nombre: 'INE' },
+      { id: 2, nombre: 'RFC' },
+    ],
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar() {
+    console.log('Guardando');
+  }
+
+  eliminar(indice: number) {
+    this.empleado.archivos.splice(indice, 1);
+  }
+
+  agregar() {
+    console.log(this.nuevoArchivo);
+    const nuevo: Documento = {
+      id: this.empleado.archivos.length + 1,
+      nombre: this.nuevoArchivo,
+    };
+    this.empleado.archivos.push(nuevo);
   }
 
 }
