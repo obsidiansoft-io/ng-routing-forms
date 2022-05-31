dd59d5025556b19299cdb829d66143a391b693ac
Ruddy García <ruddygasol94@gmail.com>
2022-05-31 12:22:43 -0500

    Adding reactive forms

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BasicosComponent implements OnInit {
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('Ruddy'),
+  //   edad: new FormControl(27)
+  // });
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [
+      'Ruddy',
+      [Validators.required, Validators.minLength(3)]
+    ],
+    edad: [
+      '',
+      [Validators.required, Validators.min(18)]
+    ],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    
   }
 
+  campoInvalido(campo: string) {
+    return this.miFormulario.controls[campo].errors
+      && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar() {
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
 }
--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DinamicosComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['Ruddy'],
+    documentos: this.fb.array(
+      [
+        [ 'RFC', Validators.required ],
+        [ 'CURP', Validators.required ],
+      ],
+      Validators.required
+    )
+  })
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  get documentosArr() {
+    return this.miFormulario.get('documentos') as FormArray;
+  }
 }
